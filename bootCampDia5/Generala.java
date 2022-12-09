045de5433ed0d1e1f27b28932d456dec2b039d51
Diego Yegros <diegoyegrosa@gmail.com>
2022-12-09 13:26:40 -0300

    bootcampDia5

---
+++ bootCampDia5/Generala.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+public class Generala
+{
+    // ESTA ES LA FUNCIÓN QUE HAY QUE IMPLEMENTAR
+    // TAMBIÉN PUEDEN AGREGAR OTRAS FUNCIONES y/o CLASES
+    // QUE NECESITEN PARA RESOLVER EL EJERCICIO DE LA
+    // MANERA MÁS ORDENADA POSIBLE
+    String jugada(String dados)
+    {
+        int iguales = 0;
+        for (int x = 0; x<datos.length(); x++){
+            if (datos[x] == datos[x+1]){
+                iguales+;
+            }
+            System.out.println(iguales);
+        }
+// ESTO SI SE EJECUTA PARA USTEDES
+    }
+    public static void main(String[] args) {
+        Generala g = new Generala();
+        g.jugada("2222")
+    }
+}
