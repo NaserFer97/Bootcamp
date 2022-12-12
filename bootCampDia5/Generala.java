50324002a1d2de4e637cf9a6846d077e6e5addd1
Diego <diegoyegrosa@gmail.com>
2022-12-12 09:43:50 -0300

    Generala with random numbers

--- bootCampDia5/Generala.java
+++ bootCampDia5/Generala.java
@@ -6,36 +6,43 @@ public class Generala
     // QUE NECESITEN PARA RESOLVER EL EJERCICIO DE LA
     // MANERA MÁS ORDENADA POSIBLE
     HashMap<Integer, Integer> Diccionario = new HashMap<>();
-    private static int numero = 0;
     private final int[] intArray = new int[5];
-    private int contadorEscalera =1;
     String jugada()
     {
+        //Se encarga de generar los dados y determinar que clase de jugada es
+        generarDados();
         if (generala()){return "GENERALA";}
         if (poker()){return "POKER";}
         if (full()){return "FULL";}
         if (escalera()){return "ESCALERA";}
-        else{return "NADA";
+        else{
+            return "NADA";
     }
-    }
-    public static boolean isInteger (String str){
-        try{
-            numero = Integer.parseInt(str);
-            return true;
-        }catch(NumberFormatException noInteger){
-            return false;
-        }
     }
     public boolean generala(){
+        //Retorna generala si los 5 dados son iguales
         return Diccionario.size() == 1;
     }
     public boolean poker(){
+        //Retorna poker si 4 dados son iguales
         return Diccionario.containsValue(4);
     }
     public boolean escalera(){
+        //Retorna escalera si los numeros son consecutivos
+        int contadorEscalera = 1;
+        for (int x =0; x<5; x++){
+            for (int j=x; j<4; j++ ){
+                if (intArray[x] == intArray[j+1]-1){
+                    contadorEscalera++;
+                } else if (intArray[x] == 6 && intArray[j+1] ==1) {
+                    contadorEscalera++;
+                }
+            }
+        }
         return contadorEscalera == 5;
     }
     public void stringToHashMap(String dados){
+        //Hashmap para obtener poker, full y generala
         int numero = Integer.parseInt(dados);
             if (Diccionario.containsKey(numero)) {
                 Diccionario.put(Integer.parseInt(dados), Integer.parseInt("1") + Diccionario.get(Integer.parseInt(dados)));}
@@ -43,26 +50,22 @@ public class Generala
             {Diccionario.put(Integer.parseInt(dados), 1);}
         }
     public boolean full() {
+        //Retorna full si 3 dados son iguales y los otros dos tambien son iguales entre ellos
         return Diccionario.size() == 2 && Diccionario.containsValue(3);
+    }
+    public void generarDados(){
+        //Genera 5 dados y los añade al array de dados
+        System.out.println("Dados");
+        for (int x = 0; x<5; x++){
+            int dado;
+            dado = 1 + (int)(Math.random() * ((6 - 1) + 1));
+            System.out.format("Dado %d: %d\n", x+1, dado);
+            intArray[x] = dado;
+            stringToHashMap(Integer.toString(dado));
+        }
     }
     public static void main(String[] args) {
         Generala g = new Generala();
-        Scanner Objeto = new Scanner(System.in);
-        String userInput;
-        System.out.println("Ingresar 5 dados ");
-            for (int x =0; x<5; x++){
-                do{
-                    System.out.format("Ingrese dado %d: ", x+1);
-                    userInput = Objeto.nextLine();
-                    g.intArray[x] = Integer.parseInt(userInput);
-                    if(!g.Diccionario.isEmpty()){
-                      if   (g.intArray[x-1] == 6 && Integer.parseInt(userInput) == 1){g.contadorEscalera++;}
-                      else if (g.intArray[x-1] == Integer.parseInt(userInput)-1){g.contadorEscalera++;}
-                    }
-                    g.stringToHashMap(userInput);
-                }while(!isInteger(userInput) || numero<1 || numero>6);
-            }
-            Objeto.close();
         System.out.println(g.jugada());
     }
 }
